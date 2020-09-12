2b74bef77f8e25351de657b1e16848f120ee0521
Jesse Vaughn <jcv221@gmail.com>
2020-09-12 02:46:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   numbers = 0
   new_array = []
   while array[numbers] < array.length do
-  new_array << array[numbers].to_i**array[numbers].to_i**array[numbers].to_i
+  new_array << array[numbers].to_i**array[numbers].to_i
   numbers += 1
 end 
 new_array
